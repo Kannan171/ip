253710767dbdfe311d7f3fd0a02dd1ab1f1edc99
Kannan171 <kannanannamalai100@gmail.com>
2025-01-23 23:20:00 +0800

    Renamed main class and added greeting

---
+++ src/main/java/Chitti.java
@@ -0,0 +1,7 @@
+public class Chitti {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Chitti");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
