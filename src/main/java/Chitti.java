6073ea5a94ba00db7ded4f5d1f5732aac26d3854
Kannan171 <kannanannamalai100@gmail.com>
2025-01-23 23:30:54 +0800

    Echo

--- src/main/java/Chitti.java
+++ src/main/java/Chitti.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Chitti {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Chitti");
         System.out.println("What can I do for you?");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        while (!input.equals("bye")){
+            System.out.println(input);
+            input = s.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
